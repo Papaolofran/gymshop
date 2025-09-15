5e4bfe9276694f3aa2114e704abd064edc3a8276
AlvaroRenzi <alvarorenzivillani@gmail.com>
2025-09-15 15:13:27 -0300

    1.3.0
    Implementacion del ProductPage y AboutUsPage

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,7 +1,33 @@
 export const AboutPage = () => {
     return (
-        <div>
-            AboutPage
+        <div className="space-y-5">
+
+            <h1 className="text-center text-4xl font-semibold tracking-tight mb-5">
+                Sobre Nosotros
+            </h1>
+
+            <img src="/img/Nosotros.jpeg" alt="Imagen de fondo" className="w-full h-auto max-h-[600px] object-cover"/>
+
+            <div className="flex flex-col gap-4 tracking-tight leading-7 text-sm font-medium text-slate-800">
+                <p>
+                    {/*PONER DESCRIPCION DE LA EMPRESA*/}
+                    GymShop es una tienda en línea que ofrece una amplia gama de productos de calidad, desde ropa deportiva hasta suplementos para mejorar tu estilo de vida.
+                </p>
+
+                <p>
+                    En GymShop, nos comprometemos a brindarte la mejor experiencia de compra. Nuestra tienda en línea ofrece una amplia gama de productos de calidad, desde ropa deportiva hasta accesorios y equipo de gimnasio. 
+                </p>
+
+                <h2 className="text-3xl font-semibold tracking-tight mt-8 mb-4">
+                    ¡No esperes más y compra tus productos en GymShop!
+                </h2>
+
+                <p>
+                    Para más información, no dudes en contactarnos a través de nuestro correo electrónico:
+                    <a href="mailto:contacto@gymshop.com"> contacto@gymshop.com</a> o llamando al número:
+                    <a href="tel:+5491122334455"> +54 9 11 2233-4455</a>
+                </p>
+            </div>
         </div>
     );
 };
--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,7 +1,41 @@
+import { allCelulares } from "../data/initialData";
+import { prepareProducts } from "../helpers";
+import { CardProduct } from "../components/shared/products/CardProduct";
+import { ContainerFilter } from "../components/shared/products/ContainerFilter";
+
 export const ProductsPage = () => {
+
+    const preparedProducts = prepareProducts(allCelulares);
     return (
-        <div>
-            ProductsPage
-        </div>
+        <>
+         <h1 className="text-5xl font-semibold text-center mb-12">
+            Productos
+         </h1>   
+
+         <div className="grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
+            {/*FILTROS*/}
+            <ContainerFilter/>
+
+            <div className="col-span-2 lg:col-span-2 xl:col-span-4 flex flex-col gap-12">
+                <div className="grid grid-cols-2 gap-3 gap-y-10 xl:grid-cols-4">
+                    {preparedProducts.map(product => (
+                        <CardProduct
+                        key={product.id}
+                        img={product.images[0]}
+                        name={product.name}
+                        price={product.price}
+                        slug={product.slug}
+                        colors={product.colors}
+                        variants={product.variants}
+                        />
+                    ))}
+                </div>
+                {/*TODO: PAGINACIÓN*/}
+                <div>
+                    
+                </div>
+            </div>
+         </div>
+        </>
     );
 };
